249d01ffa78f4ac7c90f45a33971c31da7d9e9ae
k1te <kmiller343@gmail.com>
2021-01-18 01:04:19 -0600

    very basic landing page

---
+++ client/src/components/landing/LoginButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const LoginBtn = styled(motion.button)``;
+
+const LoginButton = () => {
+  return <LoginBtn>Log in</LoginBtn>;
+};
+
+export default LoginButton;
---
+++ client/src/components/landing/SignUpButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const SignUpBtn = styled(motion.button)``;
+
+const SignUpButton = () => {
+  return <SignUpBtn>Sign up</SignUpBtn>;
+};
+
+export default SignUpButton;
