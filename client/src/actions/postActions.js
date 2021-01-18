8b14ce4aec8607297a7f17d8a2c3a514f6b8be01
k1te <kmiller343@gmail.com>
2021-01-18 14:16:44 -0600

    rm redux in favor of react context

--- client/src/actions/postActions.js
+++
@@ -1,7 +0,0 @@
-import { GET_POSTS, ADD_POST, DELETE_POST } from "./types";
-
-export const getPosts = () => {
-  return {
-    type: GET_POSTS,
-  };
-};
--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import postReducer from "./postReducer";
-import authReducer from "./authReducer";
-
-export default combineReducers({
-  post: postReducer,
-  auth: authReducer,
-});
