198d190ee158fb05b20e505a11173e934dd05284
k1te <kmiller343@gmail.com>
2021-01-18 01:19:54 -0600

    added links for login/signup button

--- client/src/components/landing/SignUpButton.js
+++ client/src/components/landing/SignUpButton.js
@@ -1,11 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
-const SignUpBtn = styled(motion.button)``;
+const SignUpBtn = styled(motion.button)`
+  width: 100%;
+`;
 
 const SignUpButton = () => {
-  return <SignUpBtn>Sign up</SignUpBtn>;
+  return (
+    <Link to="/signup">
+      <SignUpBtn>Sign up</SignUpBtn>
+    </Link>
+  );
 };
 
 export default SignUpButton;
