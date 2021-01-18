bc4adb317c180625d9492fd57008d046df261307
k1te <kmiller343@gmail.com>
2021-01-18 14:29:36 -0600

    added get route for api/auth to load logged user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,6 +6,19 @@ const jwt = require("jsonwebtoken");
 // User Model
 const User = require("../../models/User");
 
+// @route     GET api/auth
+// @desc      Get logged in user
+// @access    Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 // @route POST api/auth
 // @desc Auth user
 // @access Public
