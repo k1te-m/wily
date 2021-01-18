9808cf295ff8ac01cec45d0deca53d4988be4b9c
k1te <kmiller343@gmail.com>
2021-01-18 14:46:38 -0600

    added middleware to route for protected routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const config = require("config");
 const jwt = require("jsonwebtoken");
+const auth = require("../../middleware/auth");
 
 // User Model
 const User = require("../../models/User");
