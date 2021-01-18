83f8147e41d94b53ede130b74afe2c76404a1f6a
k1te <kmiller343@gmail.com>
2021-01-18 14:46:52 -0600

    added Auth context to app

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,29 @@ import CreatePost from "./pages/CreatePost";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-// import { Provider } from "react-redux";
-// import store from "./store";
+
+// Auth Context States
+import AuthState from "./context/auth/AuthState";
+import setAuthToken from "./utils/setAuthToken";
+
+// Put login token in local storage
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/post" component={CreatePost} />
-        </Switch>
-      </Router>
+      <AuthState>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/post" component={CreatePost} />
+          </Switch>
+        </Router>
+      </AuthState>
     </div>
   );
 }
