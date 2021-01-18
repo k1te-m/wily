08abcc4bdee5b84858df5595ac7709b6694d8c91
k1te <kmiller343@gmail.com>
2021-01-18 01:51:47 -0600

    added mongoose connection and created user model

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: "Please enter a valid password.",
+    trim: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+
+// Express Instance
 const app = express();
+
+// Variable Port
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -10,10 +19,18 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wily", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
