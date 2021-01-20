6b16659cfac779fd636d3c1d33cd372c541e1945
k1te <kmiller343@gmail.com>
2021-01-20 16:24:27 -0600

    added sort to getPosts function

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,6 +3,7 @@ const Post = require("../models/post");
 module.exports = {
   getPosts: (req, res) => {
     Post.find({})
+      .sort({ date: -1 })
       .then((posts) => res.json(posts))
       .catch((error) => res.status(422).json(error));
   },
