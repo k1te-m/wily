96b9a11084e2a5b0e118777de3f3482bb1c81fae
k1te <kmiller343@gmail.com>
2021-01-20 13:41:14 -0600

    conditional return based on if user logged in

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -3,6 +3,7 @@ import Logo from "../components/Logo";
 import LoginButton from "../components/landing/LoginButton";
 import SignUpButton from "../components/landing/SignUpButton";
 import AuthContext from "../context/auth/authContext";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   const authContext = useContext(AuthContext);
@@ -11,26 +12,41 @@ const Landing = () => {
     authContext.loadUser();
   }, []);
 
-  return (
-    <div className="container">
-      <Logo />
-      <div className="row">
-        <h3>
-          Integer mi nisl, aliquet sit amet velit ac, consectetur venenatis
-          massa.
-        </h3>
+  if (authContext.user === null) {
+    return (
+      <div className="container">
+        <Logo />
+        <div className="row">
+          <h3>
+            Integer mi nisl, aliquet sit amet velit ac, consectetur venenatis
+            massa.
+          </h3>
+        </div>
+        <div className="row">
+          <span>Join Wily Today</span>
+        </div>
+        <div className="row">
+          <SignUpButton />
+        </div>
+        <div className="row">
+          <LoginButton />
+        </div>
       </div>
-      <div className="row">
-        <span>Join Wily Today</span>
+    );
+  } else {
+    return (
+      <div className="container">
+        <div className="row">
+          <h5>Welcome, {authContext.user.name}!</h5>
+        </div>
+        <div className="row">
+          <Link to="/post">
+            <button>Create Post</button>
+          </Link>
+        </div>
       </div>
-      <div className="row">
-        <SignUpButton />
-      </div>
-      <div className="row">
-        <LoginButton />
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Landing;
