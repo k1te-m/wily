7d6051ad293d27a77a1f0ecb0c38a7bf83b33833
k1te <kmiller343@gmail.com>
2021-01-20 16:25:08 -0600

    added redirect on post POST request

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
+import { useHistory } from "react-router-dom";
 import Logo from "../components/Logo";
 import { FormBtn, Input, TextArea } from "../components/Form";
 import AuthContext from "../context/auth/authContext";
@@ -7,19 +8,24 @@ import API from "../utils/API";
 const CreatePost = (props) => {
   const authContext = useContext(AuthContext);
 
+  let history = useHistory();
+
   useEffect(() => {
     authContext.loadUser();
   }, []);
 
   const [postObject, setPostObject] = useState({
     post: "",
-    author: authContext.user._id,
+    author: authContext.user.name,
+    authorID: authContext.user._id,
   });
 
   const { post } = postObject;
 
   const createPost = (data) => {
-    API.savePost(data);
+    API.savePost(data).then(() => {
+      history.push("/");
+    });
   };
 
   const handleInputChange = (event) => {
