9ca60432aa7939609de659316d09db11d02a1029
k1te <kmiller343@gmail.com>
2021-01-20 14:38:36 -0600

    created post model/updated user model

--- models/post.js
+++ models/post.js
@@ -2,21 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-  //   user: [
-  //     {
-  //       type: Schema.Types.ObjectId,
-  //       ref: "User",
-  //     },
-  //   ],
-  //   title: {
-  //     type: String,
-  //     required: true,
-  //     trim: true,
-  //   },
   post: {
     type: String,
     required: true,
     trim: true,
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
   },
 });
 
