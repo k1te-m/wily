842645b9296aebb9e92db0f9c98057f5aafafc9b
k1te <kmiller343@gmail.com>
2021-01-20 16:24:48 -0600

    Basic Post Feed now working

--- client/src/components/landing/PostFeed.js
+++ client/src/components/landing/PostFeed.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import API from "../../utils/API";
 import AuthContext from "../../context/auth/authContext";
+import axios from "axios";
 
 const PostFeed = (props) => {
   const authContext = useContext(AuthContext);
@@ -8,13 +9,32 @@ const PostFeed = (props) => {
 
   let posts = [];
 
+  const getPosts = () => {
+    API.getPosts().then((response) => {
+      posts = response.data;
+      setWilyPosts(posts);
+      console.log(wilyPosts);
+    });
+  };
+
   useEffect(() => {
     authContext.loadUser();
+    getPosts();
   }, []);
 
   return (
     <div className="container">
       <div className="row">Post Feed</div>
+      <div className="row">
+        {wilyPosts.map((post) => (
+          <div className="card">
+            <div className="card-body">
+              <span className="card-title">{post.author}</span>
+              <p className="card-text">{post.post}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
