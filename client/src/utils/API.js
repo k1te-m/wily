44ede4c97ff0355dca66beeab190bd7d62c01bff
k1te <kmiller343@gmail.com>
2021-01-21 16:16:06 -0600

    API functions to get posts/users by id/username

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,4 +7,10 @@ export default {
   savePost: async (data) => {
     return await axios.post("/api/posts", data);
   },
+  getPostsById: async (id) => {
+    return await axios.get("/api/posts/" + id);
+  },
+  getUserByUsername: async (id) => {
+    return await axios.get("/api/users/" + id);
+  },
 };
