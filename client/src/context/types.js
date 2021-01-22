b7eeefd8de213e6b4f83bfb72f90073848b18e20
k1te <kmiller343@gmail.com>
2021-01-21 18:04:04 -0600

    added types for posts

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -11,3 +11,6 @@ export const LOGIN_FAIL = "LOGIN_FAIL";
 export const GET_POSTS = "GET_POSTS";
 export const ADD_POST = "ADD_POST";
 export const DELETE_POST = "DELETE_POST";
+export const LIKE_POST = "LIKE_POST";
+export const UNLIKE_POST = "UNLIKE_POST";
+export const SET_CURRENT_POST = "SET_CURRENT_POST";
